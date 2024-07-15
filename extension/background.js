10eab3c46c8ccf47a5a8bbd647d46a6250466a37
TonmoyDaFulkopi <1905085@ugrad.cse.buet.ac.bd>
2024-07-15 06:06:48 +0600

    Integration of extension working just fine(used last resort and closed the tab to make it easier)

--- extension/background.js
+++ extension/background.js
@@ -1,27 +1,39 @@
+// background.js
+
 chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension Installed');
-  });
-  
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log('Message received in background script', request);
-    if (request.type === 'checkFile') {
-      const fileContent = request.content;
-      console.log('Checking file content for keyword');
-      if (fileContent.includes('secretKeyword')) {
-        console.log('Keyword detected, sending notification');
-        chrome.notifications.create({
-          type: 'basic',
-          iconUrl: 'icons/icon48.png',
-          title: 'Keyword Detected',
-          message: 'The file contains the keyword: secretKeyword',
-          priority: 2
-        });
-        sendResponse({ shouldBlock: true });
-      } else {
-        console.log('Keyword not detected');
-        sendResponse({ shouldBlock: false });
-      }
-    }
+  console.log('Extension Installed');
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'checkFile') {
+
+    console.log("INSIDE CHECKFILE")
+
+    fetch('http://localhost:8080/api/check', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ fileContent: request.content })
+    })
+      .then(response => response.json())
+      .then(data => {
+        //! This comes up as NOTIFICATION
+        if (data.containsKeyword) {
+          chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icons/icon48.png',
+            title: 'Keyword Detected',
+            message: 'The file contains the keyword: secretKeyword',
+            priority: 2
+          });
+        }
+        sendResponse({ shouldBlock: data.containsKeyword });
+      })
+      .catch(error => console.error('Error:', error));
     return true;  // Required to keep the message channel open for sendResponse
-  });
-  
+  } else if (request.type === 'closeTab') {
+    chrome.tabs.remove(sender.tab.id);
+  }
+
+});
