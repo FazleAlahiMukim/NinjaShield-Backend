1ec28fae43bb31ad45499b8f04bc24c9a9ae7fe7
TonmoyDaFulkopi <1905085@ugrad.cse.buet.ac.bd>
2024-09-25 22:04:44 +0600

    committing again in the name of erlich bachman <all service active and notiUtil added>

--- src/main/java/javafest/dlpservice/service/PolicyCheckerService.java
+++ src/main/java/javafest/dlpservice/service/PolicyCheckerService.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Service
+// @Service
 public class PolicyCheckerService {
 
     private static final Logger logger = LoggerFactory.getLogger(PolicyCheckerService.class);
